b14d84366db475e2086ebd1461b1969bcc07ed6d
DANIELPACHECORINCON <danielpacherincon@gmail.com>
2022-01-21 10:39:21 -0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// alert("hola mundo!");
+// prompt("ingrese datos!");
+// confirm("seguro que quiere salir");
+
+/**
+ * 1- creamos la carpeta del proyecto ( tema libre)
+ * 2- creamos la carpeta assets (css - img)
+ * 3- creamos index.html y albun.html
+ * 4- (titulo, parrafo, imagen) todo con estilos en index
+ * 5- titulo y una tabla de 6X4 con imagenes referentes al tema
+ * w3school
+ */
